b573aa0f012f0ea030763ade2496246b09f4939d
m00ka <dmitrukoss@gmail.com>
2020-11-15 19:06:02 +0100

    Added MainApp (with Homework 1)

--- src/MainApp.java
+++ src/MainApp.java
@@ -7,11 +7,11 @@ public class MainApp {
         int c = 31;
         int d = 43;
 
-        int suma1 = a + b;
-        int suma2 = c + d;
+        int suma_ab = a + b;
+        int suma_cd = c + d;
 
         //spr. czy obie sumy są liczbami parzystymi
-        if (suma1 % 2 == 0 && suma2 % 2 == 0) {
+        if (suma_ab % 2 == 0 && suma_cd % 2 == 0) {
             System.out.println("Wszystkie liczby są parzyste");
         } else {
             System.out.println("Conajmniej jedna liczba jest liczbą nieparzystą");
