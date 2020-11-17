dde8e46df85b144deb378037ab3653be12a42422
m00ka <dmitrukoss@gmail.com>
2020-11-17 13:37:06 +0100

    Added MainApp (with Homework 1)

--- src/MainApp.java
+++ src/MainApp.java
@@ -4,17 +4,31 @@ public class MainApp {
 
         int a = 22;
         int b = 24;
-        int c = 31;
+        int c = 33;
         int d = 43;
 
         int suma_ab = a + b;
         int suma_cd = c + d;
 
+        //spr. czy suma_ab jest parzysta
+        if (suma_ab % 2 == 0) {
+            System.out.println("Suma liczb: " + a + " oraz " + b + " jest liczbą parzysta");
+        } else {
+            System.out.println("Suma liczb: " + a + " oraz " + b + " jest liczbą nieparzysta");
+        }
+
+        //spr. czy suma_cd jest parzysta
+        if (suma_cd % 2 == 0) {
+            System.out.println("Suma liczb: " + c + " oraz " + d + " jest liczbą parzysta");
+        } else {
+            System.out.println("Suma liczb: " + c + " oraz " + d + " jest liczbą nieparzysta");
+        }
+
         //spr. czy obie sumy są liczbami parzystymi
         if (suma_ab % 2 == 0 && suma_cd % 2 == 0) {
-            System.out.println("Wszystkie liczby są parzyste");
+            System.out.println("Suma liczb: " + a + ", " + b + ", " + c + " oraz " + d + " jest liczbą parzysta");
         } else {
-            System.out.println("Conajmniej jedna liczba jest liczbą nieparzystą");
+            System.out.println("Suma liczb: " + a + ", " + b + ", " + c + " oraz " + d + " jest liczbą nieparzysta");
         }
 
     }
